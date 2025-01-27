a9408f627457785e6911bf909e787b7691006fa6
Doston <dostoniskandarov0204@gmail.com>
2025-01-27 14:42:23 +0600

    shopss

--- MultiShop/shop/urls.py
+++ MultiShop/shop/urls.py
@@ -4,5 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
-    path('products/<slug:slug>', ProductByCategoryView.as_view(), name='products_by_category'),
+    path('products/by/category/<slug:slug>', ProductByCategoryView.as_view(), name='products_by_category'),
+    path('products/detail/<slug:slug>', ProductDetailView.as_view(), name='products_detail'),
 ]
--- MultiShop/shop/views.py
+++ MultiShop/shop/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import *
 
 class ProductListView(ListView):
     model = Product
     context_object_name = 'products'
-            
 
 
     def get_context_data(self, *, object_list=None, **kwargs):
@@ -25,5 +24,14 @@ class ProductByCategoryView(ProductListView):
         # products = Product.objects.filter(category__slug=self.kwargs.get('slug'))
         context['products'] = products
         return context
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'shop/detail.html'
+    context_object_name = 'product'
+
+
+
+
 
 
